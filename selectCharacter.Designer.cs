fe9ed953f1c36357cdce8a8117ca0c282f52b149
eszteryelena <51724731+eszteryelena@users.noreply.github.com>
2020-08-14 14:42:28 +1200

    Add files via upload

---
+++ selectCharacter.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace coven
+{
+    partial class selectCharacter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(426, 518);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "<";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(673, 518);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ">";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(230, 237);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(210, 210);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(699, 237);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 210);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(464, 289);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 210);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // selectCharacter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "selectCharacter";
+            this.Text = "selectcharacter";
+            this.Load += new System.EventHandler(this.selectCharacter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ selectCharacter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coven
+{
+    public partial class selectCharacter : Form
+    {
+        public selectCharacter()
+        {
+            InitializeComponent();
+        }
+
+        private void selectCharacter_Load(object sender, EventArgs e)
+        {
+           
+            pictureBox1.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\1f7e21.png");
+            pictureBox2.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\Blue-circle1.png");
+            pictureBox3.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\red-circle-md.png");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\red-circle-md.png");
+            pictureBox1.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\Blue-circle1.png");
+            pictureBox3.Image = Image.FromFile("C:\\Users\\eszterscarlettherbe\\Downloads\\1f7e21.png");
+
+            /* Bitmap red = new Bitmap("C: \\Users\\eszterscarlettherbe\\Downloads\\red-circle-md.png");
+             pictureBox3.Image = red;
+             int width = pictureBox1.Width;
+             int height = pictureBox1.Height;
+
+             Color p;
+             for (int y = 0; y < height; y++)
+             {
+                 for (int x = 0; x < width; x++)
+                 {
+                     p = red.GetPixel(x, y);
+                     int a = p.A;
+                     int r = p.R;
+                     int g = p.G;
+                     int b = p.B;
+
+
+                     int avg = (r + g + b) / 3;
+                     red.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                 }
+             }*/
+        }
+    }
+}
