bac6f20c0db3175679841fa1cc0f5b5a0ba1eee7
Eszter Scarlett-Herbert <eszterscarlettherbe@mmc.school.nz>
2020-08-21 15:01:20 +1200

    changes

--- OpeningButton.cs
+++ OpeningButton.cs
@@ -1,37 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
 
-namespace coven
+public class Class1
 {
-    class OpeningButton
-    {
-        // declare fields to use in the class
-        public int x, y, width, height;//variables for the rectangle
-        public Image planetImage;//variable for the planet's image
-
-        public Rectangle planetRec;//variable for a rectangle to place our image in
-        public int score;
-        //Create a constructor (initialises the values of the fields)
-        public OpeningButton()
-        {
-            x = 550;
-            y = 600;
-            width = 160;
-            height = 35;
-            //planetImage contains the plane1.png image
-            planetImage = Properties.Resources.buttontext;
-            planetRec = new Rectangle(x, y, width, height);
-        }
-
-        public void DrawPlanet(Graphics g)
-        {
-            g.DrawImage(planetImage, planetRec);
-        }
-
-
-    }
+	public Class1()
+	{
+	}
 }
